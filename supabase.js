31df0008c4ef222b90ac1fbde6fda71250006058
XOFTHOR <fadhlifajrial77@gmail.com>
2025-12-03 13:35:11 +0700

    update

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js (ES module)
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+export const SUPABASE_URL = 'https://puzefddnrbrryspquxaj.supabase.co'; // <-- ganti
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1emVmZGRucmJycnlzcHF1eGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MzI1NDYsImV4cCI6MjA3OTIwODU0Nn0.7efR2vRAiqlYvynMLVImNpcMHirD5vJobmC111Pc1as'; // <-- ganti
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
