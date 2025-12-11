96a2ed410fea21e4b4f875d7f9b6b957bc55b74b
XOFTHOR <fadhlifajrial77@gmail.com>
2025-12-11 14:15:52 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js (ES module)
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-export const SUPABASE_URL = 'https://puzefddnrbrryspquxaj.supabase.co'; // <-- ganti
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1emVmZGRucmJycnlzcHF1eGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MzI1NDYsImV4cCI6MjA3OTIwODU0Nn0.7efR2vRAiqlYvynMLVImNpcMHirD5vJobmC111Pc1as'; // <-- ganti
+export const SUPABASE_URL = 'https://blytomdbwiiettetaxqg.supabase.co'; // <-- ganti
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJseXRvbWRid2lpZXR0ZXRheHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNzc3ODIsImV4cCI6MjA3ODk1Mzc4Mn0.coVoADR6MKfURTOlPFD-mAiZaxBABYTb6CD7W5AtKfs'; // <-- ganti
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
