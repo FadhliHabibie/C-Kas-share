9acaa40680fdd11863d0e64eea5c424bd74a8623
XOFTHOR <fadhlifajrial77@gmail.com>
2025-12-11 14:32:13 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // supabase.js (ES module)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.4/dist/esm/supabase.js';
 
 export const SUPABASE_URL = 'https://blytomdbwiiettetaxqg.supabase.co'; // <-- ganti
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJseXRvbWRid2lpZXR0ZXRheHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNzc3ODIsImV4cCI6MjA3ODk1Mzc4Mn0.coVoADR6MKfURTOlPFD-mAiZaxBABYTb6CD7W5AtKfs'; // <-- ganti
